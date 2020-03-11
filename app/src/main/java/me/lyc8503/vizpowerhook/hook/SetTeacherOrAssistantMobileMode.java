e789f532452b8fd483e85155803b2980dcfad012
Anguei <Anguei@users.noreply.github.com>
2020-03-11 20:49:44 +0800

    教师模式的启动加入 UI 开关 Checkbox

--- app/src/main/java/me/lyc8503/vizpowerhook/hook/SetTeacherOrAssistantMobileMode.java
+++ app/src/main/java/me/lyc8503/vizpowerhook/hook/SetTeacherOrAssistantMobileMode.java
@@ -1,17 +1,25 @@
 package me.lyc8503.vizpowerhook.hook;
 
 import de.robv.android.xposed.XposedBridge;
-
 import de.robv.android.xposed.XC_MethodHook;
+
+import android.app.AndroidAppHelper;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 
 public class SetTeacherOrAssistantMobileMode extends XC_MethodHook {
     @Override
     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-        super.beforeHookedMethod(param);
-        param.setResult(true);
-
-        // Context vizContext = ((Activity) param.thisObject).getApplicationContext();
-        // Toast.makeText(vizContext, "VizpowerHook: 已经启动教师模式！", Toast.LENGTH_LONG).show();
-        XposedBridge.log("已经启动教师模式！");
+        ContentResolver resolver = AndroidAppHelper.currentApplication().getApplicationContext().getContentResolver();
+        Uri uri = Uri.parse("content://me.lyc8503.vizpowerhook/config");
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        boolean teacherMode = Boolean.parseBoolean(cursor.getString(cursor.getColumnIndex("teacherMode")));
+        if (teacherMode) {
+            super.beforeHookedMethod(param);
+            param.setResult(true);
+            XposedBridge.log("已经启动教师模式！");
+        }
     }
 }
